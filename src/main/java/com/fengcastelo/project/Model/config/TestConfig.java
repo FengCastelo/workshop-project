73894c3659269746429482d9f1f70ab1997ed174
FengCastelo <ianfcastelo7@gmail.com>
2025-11-02 13:09:06 -0300

    Category Entity

--- src/main/java/com/fengcastelo/project/Model/config/TestConfig.java
+++ src/main/java/com/fengcastelo/project/Model/config/TestConfig.java
@@ -1,8 +1,10 @@
 package com.fengcastelo.project.Model.config;
 
+import com.fengcastelo.project.Model.Entities.Category;
 import com.fengcastelo.project.Model.Entities.Order;
 import com.fengcastelo.project.Model.Entities.User;
 import com.fengcastelo.project.Model.Entities.enums.OrderStatus;
+import com.fengcastelo.project.Repositories.CategoryRepository;
 import com.fengcastelo.project.Repositories.OrderRepository;
 import com.fengcastelo.project.Repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,7 +14,6 @@ import org.springframework.context.annotation.Profile;
 
 import java.time.Instant;
 import java.util.Arrays;
-import java.util.List;
 
 @Configuration
 @Profile("test")
@@ -24,8 +25,17 @@ public class TestConfig implements CommandLineRunner {
     @Autowired
     private OrderRepository orderRepository;
 
+    @Autowired
+    private CategoryRepository categoryRepository;
+
     @Override
     public void run(String... args) throws Exception {
+
+        Category cat1 = new Category(null, "Electronics");
+        Category cat2 = new Category(null, "Books");
+        Category cat3 = new Category(null, "Computers");
+
+        categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
 
         User u1 = new User("maria@gmail.com", null,"Maria Brown",  "123456", "988888888");
         User u2 = new User("alex@gmail.com", null,"Alex Green",  "123456", "977777777");
