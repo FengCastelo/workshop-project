2e44fa3a4137a45861a0cf02e018b3550a708bfa
FengCastelo <ianfcastelo7@gmail.com>
2025-11-05 20:50:21 -0300

    Users INSERT

--- src/main/java/com/fengcastelo/project/Services/UserService.java
+++ src/main/java/com/fengcastelo/project/Services/UserService.java
@@ -4,6 +4,7 @@ import com.fengcastelo.project.Model.Entities.User;
 import com.fengcastelo.project.Repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 import java.util.Optional;
@@ -21,5 +22,9 @@ public class UserService {
     public User findById(Long id) {
          Optional<User> obj = userRepository.findById(id);
          return obj.get();
+    }
+
+    public User insert(User obj){
+        return userRepository.save(obj);
     }
 }
